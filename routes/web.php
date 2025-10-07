7097a1118e941923e85eea00a8ba1f2ac9691424
IdoRefaelSiregar24 <ido24si@mahasiswa.pcr.ac.id>
2025-10-07 10:33:36 +0700

    Menggunakan Template Admin Volt

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MatakuliahController;
 
 
@@ -48,3 +49,7 @@ Route::get('/home', [HomeController::class,'index'])->name('home');
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+
+//Route untuk admin
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
