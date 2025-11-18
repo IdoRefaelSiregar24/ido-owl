9a25ed47f5a1500ec061de6409273be609ca1873
IdoRefaelSiregar24 <ido24si@mahasiswa.pcr.ac.id>
2025-11-18 07:26:00 +0700

    Penambahan Fitur Pagination

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -96,6 +96,9 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        <div class="mt-3">
+                            {{ $dataPelanggan->links('pagination::bootstrap-5') }}
+                        </div>
                     </div>
                 </div>
             </div>
